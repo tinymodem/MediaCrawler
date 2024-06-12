941e04acb959fa5a950203bdfa9dd8fef2764072
chjinche <chjinche@outlook.com>
2024-06-12 15:48:11 +0800

    change intermediate result saving dir

--- zhihu_postprocess.py
+++ zhihu_postprocess.py
@@ -32,23 +32,26 @@ def contains_phrase(data, phrase):
 
 
 def should_truncate(file_index, truncate_index):
-    return truncate_index > -1 and file_index >= truncate_index
+    return (truncate_index > -1 and file_index >= truncate_index) or file_index > 18
 
 
-def filter_more_answers(screenshots_dir):
+def filter_more_answers(screenshots_dir, output_zip_dir):
     truncate_index = -1
-    (screenshots_dir / "truncated").mkdir(parents=True, exist_ok=True)
+    truncated_dir = Path('truncated')
+    truncated_dir.mkdir(parents=True, exist_ok=True)
     for file_path in screenshots_dir.glob('*.png'):
         print(f"Checking {file_path} for more answers...")
         file_path_index = int(file_path.stem)
+        (truncated_dir / screenshots_dir.name).mkdir(parents=True, exist_ok=True)
+        truncated_path = truncated_dir / screenshots_dir.name / file_path.name
         if should_truncate(file_path_index, truncate_index):
             print(f"Moving {file_path} to truncated directory...")
-            file_path.rename(screenshots_dir / "truncated" / file_path.name)
+            file_path.rename(truncated_path)
             continue
         if contains_phrase(json.loads(ocr(file_path)), "更多回答"):
             truncate_index = int(file_path.stem)
             print(f"Found more answers at index {truncate_index}. Truncating...")
-            file_path.rename(screenshots_dir / "truncated" / file_path.name)
+            file_path.rename(truncated_path)
 
     # Create a zip file of all png files in the current directory
     zip_file_name = f'{screenshots_dir.name}.zip'
@@ -59,21 +62,20 @@ def filter_more_answers(screenshots_dir):
     print(f"All PNG files have been added to {zip_file_name}")
 
     # Ensure the output/zip directory exists
-    output_zip_dir = Path('./output/zip')
     output_zip_dir.mkdir(parents=True, exist_ok=True)
     
     # Move the zip file to the output/zip directory
     shutil.move(str(screenshots_dir / zip_file_name), str(output_zip_dir / zip_file_name))
 
 
-def batch_filter_more_answers(screenshots_dir):
+def batch_filter_more_answers(screenshots_dir, output_zip_dir):
     for screenshots_subdir in screenshots_dir.iterdir():
         if screenshots_subdir.is_dir():
-            filter_more_answers(screenshots_subdir)
+            filter_more_answers(screenshots_subdir, output_zip_dir)
             print(f"Filtered more answers for {screenshots_subdir.name}")
 
 
 if __name__ == '__main__':
     # screenshots_path = Path('./output/18050858/')
     # filter_more_answers(screenshots_path)
-    batch_filter_more_answers(Path('./output/'))
+    batch_filter_more_answers(Path('./output/'), Path('/data/share/data/zhihu/zip/0611/'))
